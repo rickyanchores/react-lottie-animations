081688b0864c7ea8a140a0296303eaa8acdb7827
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-05-20 21:08:28 +0100

    vite + sass + tailwindcss setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h3 className="font-bold text-red-400">Hello</h3>
+    </div>
+  )
+}
+
+export default App
