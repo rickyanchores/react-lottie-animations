ca66f0c241b6769feba98ac20fac87e564ae08af
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-05-21 19:53:45 +0100

    added cards

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,30 @@ import Card from './Components/Card/Card'
 import nintendoSwitch from "./animations/nintendo-switch.json"
 import ps5 from "./animations/ps5.json";
 import guitarist from "./animations/guitarist.json";
+import webdev from "./animations/webdev.json";
+import dataanalysis from "./animations/dataanalysis.json";
+import profile from "./animations/profile.json";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h3 className="font-bold text-red-400">
+      <h3 className="font-bold text-blue-400 text-lg">
         React + Vite + Lottie animation
       </h3>
       <div className="container flex flex-wrap justify-around">
+        <Card className="profile-card" title={"Profile"} text={"My personal portofolio"} animationData={profile}/>
+        <Card title={"Web Dev"} text={"Web Development"} animationData={webdev}/>
+        <Card title={"Data Analysis"} text={"Data Analyisis"} animationData={dataanalysis}/>
         <Card title={"Nintendo Switch."} text={"Test with Lottie"} animationData={nintendoSwitch}/>
         <Card title={"Playstation 5."} text={"Test with Lottie"} animationData={ps5}/>
         <Card title={"Guitarist."} text={"Test with Lottie"} animationData={guitarist}/>
       </div>
+      <p className='font-bold'>dev by Ricky A.</p>
     </div>
   )
 }
 
 export default App
+  
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,12 +4,12 @@ import Lottie from "lottie-react"
 //import animation from "../../animations/nintendo-switch.json"
 const Card = ({title,text,animationData}) => {
   return (
-    <div className='Card bg-slate-700 p-5 w-100 rounded-lg text-start m-5'>
+    <div className='Card bg-slate-700 p-5 w-120 rounded-lg text-start m-5 xl:flex'>
         <Lottie animationData={animationData}/>
-        <div className="bg-slate-600 p-5 rounded-md">
+        <div className="bg-slate-600 text-center p-5 rounded-md xl:ml-5">
             <h3 className='font-bold'>{title}</h3>
             <p>{text}</p>
-            <button>Read More</button>
+            <button className="bg-slate-900 mt-2">Read More</button>
         </div>
     </div>
   )
